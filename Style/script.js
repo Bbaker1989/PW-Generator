7b61fb2a082a55e1b8c66b3d86e297e19cacff27
bbaker1989 <brittneybaker@Brittneys-Air.lan>
2021-09-18 19:17:34 -0400

    question prompts

--- Style/script.js
+++ Style/script.js
@@ -1,5 +1,21 @@
 // Assignment code here
 
+//Prompts to generate password
+function generatePassword() {
+// password length
+var passwordLength = prompt("Please enter number of characters required for password. Password must be between 8 and 128 characters.");
+// numbers in password
+var numbers = confirm("Would you like to include numbers in your password?");
+// uppercase letters
+var upperCaseLetters = confirm("Would you like to include upper case letters?");
+// lowercase letters
+var lowerCaseLetters = confirm("Would you like to include lower case letters?");
+//special characters
+var specialCharacters = confirm("Would you like to include special characters?");
+}
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
