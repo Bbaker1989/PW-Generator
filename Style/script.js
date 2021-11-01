43a039ce2d866a6d4ceaa2afcfb9444da92e1255
bbaker1989 <brittneybaker@Brittneys-Air.lan>
2021-10-31 21:29:09 -0400

    adds or exlcudes uppper case letters

--- Style/script.js
+++ Style/script.js
@@ -1,7 +1,7 @@
 // UpperCase Letters
-var upperCase = ["Q", "W", "E", "R", "T","Y","U","I","O","P","A","S","D","F","G", "H", "J", "K", "L","Z","X","C","V","B","N","M"];
+var upperCaseLetters = ["Q", "W", "E", "R", "T","Y","U","I","O","P","A","S","D","F","G", "H", "J", "K", "L","Z","X","C","V","B","N","M"];
 //LowerCase Letters
-var lowerCase = ["q", "w", "e", "r", "t","y","u","i","o","p","a","s","d","f","g", "h", "j", "k", "l","z","x","c","v","b","n","m"];
+var lowerCaseLetters = ["q", "w", "e", "r", "t","y","u","i","o","p","a","s","d","f","g", "h", "j", "k", "l","z","x","c","v","b","n","m"];
 //Numbers
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0" ]
 //Special Characters
@@ -19,7 +19,42 @@ function generatePassword() {
     alert ("The password must be between 8 and 128 characters.");
 
     console.log(pwLength);
+
   }
+  window.alert("Your password will be " + pwLength + " characters long.");
+
+  // Include upperCase Letters
+  var pwUpperCase = window.confirm("Would you like to include upper case letters?");
+
+  // Include lowerCase Letters
+  var pwLowerCase = window.confirm("Would you like to include lower case letters?");
+
+  // Include numbers
+  var pwNumbers = window.confirm("Would you like to include numbers?");
+
+  //Include special character
+  var pwSpecialChar =window.confirm("Would you like to include special characters?");
+
+
+// Store Password
+ var genPassword = [];
+
+//Generates password with uppercase letters
+  if (pwUpperCase) {
+
+    genPassword = genPassword.concat(upperCaseLetters);
+
+    console.log("Upper case letters are included in password.");
+
+    console.log(passwordUpperChar);
+
+// Generates password without upper case letters
+  } else {
+
+    console.log("No upper case letters added.");
+    }
+
+
 
 
 }
